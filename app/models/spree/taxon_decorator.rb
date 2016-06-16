575cfefd0a6529a304d24cf5bb5ca61606c051fa
hoangnghiem <hoangnghiem1711@gmail.com>
2016-06-16 14:35:24 +0700

    Remove reject banner blank

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   Taxon.class_eval do
     has_many :banners, class_name: 'Spree::TaxonBanner', dependent: :destroy
-    accepts_nested_attributes_for :banners, reject_if: proc { |attributes| attributes['banner'].blank? }, allow_destroy: true
+    accepts_nested_attributes_for :banners, allow_destroy: true
   end
 end
